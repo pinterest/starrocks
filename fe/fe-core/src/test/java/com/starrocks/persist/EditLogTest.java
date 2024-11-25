f9c016146694c216043f72e66f97118d0e7cd2e2
Connor Brennan <cbrennan@pinterest.com>
2024-11-25 16:26:08 -0500

    fix editlog test

--- fe/fe-core/src/test/java/com/starrocks/persist/EditLogTest.java
+++ fe/fe-core/src/test/java/com/starrocks/persist/EditLogTest.java
@@ -29,6 +29,7 @@ import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.server.NodeMgr;
 import com.starrocks.system.ComputeNode;
 import com.starrocks.system.Frontend;
+import com.starrocks.system.ResourceIsolationGroupUtils;
 import com.starrocks.system.SystemInfoService;
 import com.starrocks.system.TabletComputeNodeMapper;
 import mockit.Expectations;
@@ -186,7 +187,7 @@ public class EditLogTest {
         feToWrite.setResourceIsolationGroup("somegroup2");
         JournalEntity journal = new JournalEntity();
         journal.setData(feToWrite);
-        journal.setOpCode(OperationType.OP_UPDATE_FRONTEND);
+        journal.setOpCode(OperationType.OP_UPDATE_FRONTEND_V2);
         EditLog editLog = new EditLog(null);
         editLog.loadJournal(mgr, journal);
         List<Frontend> updatedFrontends = mgr.getNodeMgr().getFrontends(null);
@@ -211,7 +212,8 @@ public class EditLogTest {
         TabletComputeNodeMapper tabletComputeNodeMapper = systemInfoService.internalTabletMapper();
         new Expectations(tabletComputeNodeMapper) {
             {
-                tabletComputeNodeMapper.modifyComputeNode(cnToWrite.getId(), null, "somegroup");
+                tabletComputeNodeMapper.modifyComputeNode(cnToWrite.getId(),
+                        ResourceIsolationGroupUtils.DEFAULT_RESOURCE_ISOLATION_GROUP_ID, "somegroup");
                 minTimes = 1;
             }
         };
