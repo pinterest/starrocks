951a177db244cecc4c2d43536a2dac2308cc6818
Slava Kovalenko <skovalenko@pinterest.com>
2026-03-31 11:50:04 +0200

    Cherry-pick CSV parser for GIN (RTA-8526) Appply "[Feature] Add CSV parser for GIN"/"pinterest-integration-3.5" changes to "pinterest-integration-4.1.0-rc" https://github.com/pinterest/starrocks/pull/79

--- be/src/storage/index/inverted/inverted_index_common.h
+++ be/src/storage/index/inverted/inverted_index_common.h
@@ -35,6 +35,7 @@ enum class InvertedIndexParserType {
 
 const std::string INVERTED_IMP_KEY = "imp_lib";
 const std::string TYPE_CLUCENE = "clucene";
+const std::string TYPE_BUILTIN = "builtin";
 const std::string INVERTED_INDEX_PARSER_KEY = "parser";
 const std::string INVERTED_INDEX_PARSER_UNKNOWN = "unknown";
 const std::string INVERTED_INDEX_PARSER_NONE = "none";
